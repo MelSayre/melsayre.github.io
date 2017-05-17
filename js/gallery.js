79d63d3bc28d9afa90d47d9ee89578f4bede9647
Taher Suterwala <tsuterwa@amazon.com>
2017-05-17 16:48:17 -0700

    Got modal to work

--- js/gallery.js
+++ js/gallery.js
@@ -1,36 +1,21 @@
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
+// Get the modal
+var modal = document.getElementById('myModal');
 
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = $('.myImg');
+var modalImg = $("#img01");
+var captionText = document.getElementById("caption");
+img.click(function(){
+  modal.style.display = "block";
+  var newSrc = this.src;
+  modalImg.attr('src', newSrc);
+  captionText.innerHTML = this.alt;
+});
 
-var slideIndex = 1;
-showSlides(slideIndex);
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
